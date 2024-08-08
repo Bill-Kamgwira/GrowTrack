98388c4431acc7259e03266ced8d2f90dc09e35e
Bill-Kamgwira <bkamgwira1@gmail.com>
2024-08-08 16:47:15 +0200

    Fixed the  tabs issue
    Fixed the  tabs issue

--- static/tabs.js
+++ static/tabs.js
@@ -1,4 +1,3 @@
-alert("Javascript Loaded!");
 const tabs = document.querySelectorAll('.tabs li a');
 const tabContent = document.querySelectorAll('.tab-pane');
 
@@ -10,13 +9,12 @@ tabs.forEach(tab => {
     e.preventDefault();
     const target = e.target.getAttribute('href');
 
-    // Remove active class from all tabs and tab content
+    // Reset all tabs and content to inactive state
     tabs.forEach(tab => tab.classList.remove('active'));
-    tabContent.forEach(tab => tab.classList.remove('active'));
+    tabContent.forEach(content => content.classList.remove('active'));
 
-    // Add active class to the clicked tab and its corresponding content
+    // Activate the clicked tab and its corresponding content
     e.target.classList.add('active');
     document.querySelector(target).classList.add('active');
   });
 });
-
